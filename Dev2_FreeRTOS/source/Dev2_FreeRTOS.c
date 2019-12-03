b89fc9cd37c956f5dff804069e06ffbe11237b77
Zedd83 <zkrousky@gmail.com>
2019-12-03 11:31:26 +0000

    main task created
    main task created, red and green leds configured

--- Dev2_FreeRTOS/source/Dev2_FreeRTOS.c
+++ Dev2_FreeRTOS/source/Dev2_FreeRTOS.c
@@ -1,37 +1,11 @@
-/*
- * Copyright 2016-2019 NXP
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of NXP Semiconductor, Inc. nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
- 
 /**
  * @file    Dev2_FreeRTOS.c
  * @brief   Application entry point.
  */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+
 #include <stdio.h>
 #include "board.h"
 #include "peripherals.h"
@@ -39,13 +13,13 @@
 #include "clock_config.h"
 #include "MK64F12.h"
 #include "fsl_debug_console.h"
-/* TODO: insert other include files here. */
 
-/* TODO: insert other definitions and declarations here. */
+#define DEBUG_MSG 1
 
-/*
- * @brief   Application entry point.
- */
+void mainTask(void*);
+
+TaskHandle_t mainTaskHandle = NULL;
+
 int main(void) {
 
   	/* Init board hardware. */
@@ -55,16 +29,31 @@ int main(void) {
   	/* Init FSL debug console. */
     BOARD_InitDebugConsole();
 
-    PRINTF("Hello World\n");
+#ifdef DEBUG_MSG
+    PRINTF("\033[32mApplication Start\033[0m\n\r");
+#endif
 
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
-    while(1) {
-        i++ ;
-        /* 'Dummy' NOP to allow source level single stepping of
-            tight while() loop */
-        __asm volatile ("nop");
+    if(xTaskCreate(mainTask, "Main Task", configMINIMAL_STACK_SIZE + 20, NULL, 3, &mainTaskHandle) == pdFALSE)
+    {
+    	PRINTF("\n\r\033[31mMain Task creation failed\033[0m\n\r");
     }
+
+    vTaskStartScheduler();
+
+    while(1) {}
+
     return 0 ;
+}
+
+void mainTask(void* pvParameters)
+{
+#ifdef DEBUG_MSG
+	PRINTF("\n\rMain Task\n\r");
+#endif
+
+	for(;;)
+	{
+		GPIO_PortToggle(BOARD_RED_LED_GPIO, 1 << BOARD_RED_LED_PIN);
+		vTaskDelay(pdMS_TO_TICKS(1000));
+	}
 }
